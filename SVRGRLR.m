746eee3b434b859402c1e17935ea9e7a4af66ee2
Nat <65334078+Nat-aho@users.noreply.github.com>
2020-05-16 17:33:13 +0200

    Update SVRGRLR.m

--- SVRGRLR.m
+++ SVRGRLR.m
@@ -1,5 +1,5 @@
 function [w,wVec,it,loss,ttot,lossVec,timeVec,gnrit,err] = SVRGRLR(X,y,w,reg,lc,...
-    verbosity,nepochs,maxit)
+          nepochs,maxit)
 
 %----------------------------------------------------------------------------------
 % Stochastic Variance Reduction Gradient Method for Regularized Logistic Regression
