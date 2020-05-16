17e43befcbfc5175bb9875f19849ed84c62302cd
Claudia <65338398+coclab@users.noreply.github.com>
2020-05-16 18:34:47 +0200

    Update MainHW1.m

--- MainHW1.m
+++ MainHW1.m
@@ -1,54 +1,59 @@
-%HOMEWORK 1 - OPTIMIZATION FOR DATA SCIENCE - 15/5/2020
+% HOMEWORK 1 - OPTIMIZATION FOR DATA SCIENCE - 17/5/2020
 
-%Caria Natascia
-%Cozzolino Claudia
-%Petrella Alfredo
+% Caria Natascia - 1225874
+% Cozzolino Claudia - 1227998
+% Petrella Alfredo - 1206627
 
-%IMPORT DATASET AND PREPROCESS
+% IMPORT DATASET AND PREPROCESS
 
 datagis = load('data.mat');
-data=datagis.gis;
+data = datagis.gis;
 
 % Train-test split
-X_train=[data.Xtrain ones(size(data.Xtrain,1),1)];
-X_test= [data.Xtest ones(size(data.Xtest,1),1)];
+X_train = [data.Xtrain ones(size(data.Xtrain,1),1)];
+X_test = [data.Xtest ones(size(data.Xtest,1),1)];
 y_train = data.ytrain;
 y_test = data.ytest;
 
 [m, n] = size(X_train);
 
-%REGULARIZED LOGISTIC REGRESSION
+% REGULARIZED LOGISTIC REGRESSION
 
-%Initilize hyper-parameters
+% initilize hyper-parameters
 
-%initialize parameters to Std Normal random values
-w_gm=randn(1,n);
-w_sg=randn(1,n);
-w_svrg=randn(1,n);
+% initialize parameters to Std Normal random values
+w_gm = randn(1,n);
+w_sg = randn(1,n);
+w_svrg = randn(1,n);
 
-%min-max normalization in [-1 , 1]
+% min-max normalization in [-1 , 1]
 w_gm = w_gm/max(abs(w_gm));  
 w_sg = w_sg/max(abs(w_sg));
 w_svrg = w_svrg/max(abs(w_svrg));
 
-%initilize hyper-parameters
-reg_gm=0.01;
-reg_sg=10;
-reg_svrg=0.01;
+% initilize hyper-parameters
+reg_gm = 0.01;
+reg_sg = 10;
+reg_svrg = 0.01;
 
-%Lipschitz constants extimation
-L=10^6;
-LC=0.001;
+% constants
+L_gm = 10^6;        % Lipschitz constant over estimastion (GD method)
+LC_sg = 0.001;      % step size numerator (SGD method)
+alpha_svrg = 0.001; % step size (SVRG method)
 
-%maximum number of iterations (use multiple of 100 for the print function)
-maxit_gm=1000;
-maxit_sg=50000;
+% maximum number of iterations (use multiple of 100 for the print function)
+maxit_gm = 1000;
+maxit_sg = 50000;
 maxit_svrg = 50000;
 
-%output type
-verbosity=0;
+% loss and weight update rate for accuracy computation
+rate_gm = 1000;
+rate_sg = 1000;
 
-%class all open figures (if any)
+% epochs length (SVRG method)
+eplen_svrg = 1000;
+
+% class all open figures (if any)
 close all
 
 %1) GRADIENT DESCENT FIXED STEPSIZE
@@ -57,14 +62,13 @@ disp('*        GM STANDARD        *');
 disp('*****************************');
 
 %call GD_rlr
-%tolerance for the stopping condition on the gradient norm
-eps_gm=1.0e-1;
 
 [optw_gm,wVec_gm,it_gm,loss_gm,ttot_gm,lossVec_gm,timeVec_gm,gnrit_gm,err_gm] = ...
-GDRLR(X_train,y_train,w_gm,reg_gm,L,maxit_gm,eps_gm,verbosity);
+GDRLR(X_train,y_train,w_gm,reg_gm,L_gm,maxit_gm,rate_gm);
 
 % Print results:
-[t_gm,accVec_gm,F1Vec_gm]=PrintResults('GM',X_train,X_test,y_train,y_test,optw_gm,wVec_gm,it_gm,loss_gm,ttot_gm,lossVec_gm,timeVec_gm,gnrit_gm,err_gm);
+[t_gm,accVec_gm,F1Vec_gm]=PrintResults('GM',X_train,X_test,y_train,y_test,optw_gm,...
+wVec_gm,it_gm,loss_gm,ttot_gm,lossVec_gm,timeVec_gm,gnrit_gm,err_gm);
 
 %2) STOCHASTIC GRADIENT DESCENT
 
@@ -72,12 +76,13 @@ disp('*****************************');
 disp('*        SGM STANDARD       *');
 disp('*****************************');
 
-%call STG_rlr
-[optw_sg,wVec_sg,it_sg,loss_sg,ttot_sg,lossVec_sg,timeVec_sg,gnrit_sg,err_sg] = ...
-SGRLR(X_train,y_train,w_sg,reg_sg,LC,maxit_sg,verbosity);
+% call STG_rlr
+[optw_sg,wVec_sg,it_sg,loss_sg,ttot_sg,lossVec_sg,timeVec_sg,err_sg] = ...
+SGRLR(X_train,y_train,w_sg,reg_sg,LC_sg,maxit_sg,rate_sg);
 
 % Print results:
-[t_sg,accVec_sg,F1Vec_sg]=PrintResults('SGM',X_train,X_test,y_train,y_test,optw_sg,wVec_sg,it_sg,loss_sg,ttot_sg,lossVec_sg,timeVec_sg,gnrit_sg,err_sg);
+[t_sg,accVec_sg,F1Vec_sg]=PrintResults('SGM',X_train,X_test,y_train,y_test,optw_sg,...
+wVec_sg,it_sg,loss_sg,ttot_sg,lossVec_sg,timeVec_sg,err_sg);
 
 %3) SVRG
 
@@ -86,10 +91,9 @@ disp('*       SVRGM STANDARD      *');
 disp('*****************************');
 
 %call SVRG_rlr
-nepochs=1000;
 
-[optw_svrg,wVec_svrg,it_svrg,loss_svrg,ttot_svrg,lossVec_svrg,timeVec_svrg,gnrit_svrg,err_svrg] = SVRGRLR(X_train,y_train,w_svrg,reg_svrg,LC,...
-    verbosity,nepochs,maxit_svrg);
+[optw_svrg,wVec_svrg,it_svrg,loss_svrg,ttot_svrg,lossVec_svrg,timeVec_svrg,gnrit_svrg,err_svrg] = SVRGRLR(X_train,y_train,w_svrg,...
+reg_svrg,alpha_svrg,eplen_svrg,maxit_svrg);
 
 % Print results:
 [t_svrg,accVec_svrg,F1Vec_svrg]=PrintResults('SVRG',X_train,X_test,y_train,y_test,optw_svrg,wVec_svrg,it_svrg,loss_svrg,ttot_svrg,lossVec_svrg,timeVec_svrg,gnrit_svrg,err_svrg);
