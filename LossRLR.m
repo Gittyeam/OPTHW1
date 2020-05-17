aeaf8d73a83219d7928024c5f7a5ff9c52970a43
Nat <65334078+Nat-aho@users.noreply.github.com>
2020-05-17 20:57:23 +0200

    Update LossRLR.m

--- LossRLR.m
+++ LossRLR.m
@@ -1,19 +1,20 @@
 
 function [loss]=LossRLR(X,y,w,reg)
 
-%------------------------------------------------------------------
-%RLR LOSS FUNCTION
-%------------------------------------------------------------------
+%------------------------------------------------------------------------
+% RLR LOSS FUNCTION
+%------------------------------------------------------------------------
 
-%INPUT
-%X: matrix of sizes (m,n), m istances of dimension n
-%y: col vector of length m, it contains the corresponding label for each
-%istance in X (binary classification -1/+1)
-%w: row vector of length n, parameter
-%reg: scalar, regularization term
+% INPUT
+% X: matrix of sizes (m,n), m istances of dimension n
+% y: col vector of length m, it contains the corresponding label for each
+% istance in X (binary classification -1/+1)
+% w: row vector of length n, parameter
+% reg: scalar, regularization term
 
-%OUTPUT
-%loss: loss function value (scalar)
+% OUTPUT
+% loss: loss function value (scalar)
+%------------------------------------------------------------------------
 
 loss=0;
 for i=1:size(X,1)
@@ -23,4 +24,5 @@ for i=1:size(X,1)
     loss=loss+l;
 end
 loss=loss+(reg/2)*(w*w');
+
 end
